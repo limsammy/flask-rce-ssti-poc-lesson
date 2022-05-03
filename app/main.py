0bcc46588587f713ffd58143ce8845c5a4ca7a0c
s.lim <sam@typhonic.io>
2022-05-02 18:50:31 -0700

    Fix poc for working template injection.

--- app/main.py
+++ app/main.py
@@ -5,5 +5,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    name = request.args.get("name") or None  # get untrusted query param
-    return render_template("index.html", name=name)  # render it into template
+    name = request.args.get("name") or None
+    if name:
+        template = (
+            """<p> Hello, %s 
+        """
+            % name
+        )
+
+    else:
+        template = """
+        <p> What yor name? </p>
+        <form method="get" action="/">
+        <p>
+            Enter your name:
+            <input type="text" name="name">
+        </p>
+        <input type="submit" value="Submit">
+        </form>"""
+
+    return render_template_string(template, name=name)
+
+    # name = request.args.get("name") or None  # get untrusted query param
+    # return render_template("index.html", name=name)  # render it into template
